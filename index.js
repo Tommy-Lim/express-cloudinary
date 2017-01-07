ccd48ce9c4a9428c0367341010e15fa013d94dd3
Tommy-Lim <tlim24@gmail.com>
2017-01-06 16:06:15 -0800

    finished file upload to cloudinary module

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
+var cloudinary = require('cloudinary');
+var fs = require('fs');
+
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -7,6 +13,19 @@ app.use(ejsLayouts);
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.post('/', upload.single('myFile'), function(req, res){
+  // upload that image to cloudinary
+  cloudinary.uploader.upload(req.file.path, function(result){
+    fs.unlink(req.file.path, function(err){
+      if(err){
+        res.send(err);
+      } else{
+        res.send('image was uploaded to cloudinary and deleted locally');
+      }
+    });
+  });
 });
 
 app.listen(3000);
